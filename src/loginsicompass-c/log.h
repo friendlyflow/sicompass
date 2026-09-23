477f44eab13cc218f606209a3969a2be788de3b4
Nico Verrijdt <nicoverrijdt@gmail.com>
2026-09-23 12:54:47 +0200

    Remove obsolete C sources (desicompass-c, loginsicompass-c)
    Both greeters were rewritten in Rust; these C trees have been unused
    since the rewrite and are no longer referenced by the flake or build.

--- src/loginsicompass-c/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOG_H
-#define LOG_H
-
-void log_error(const char *const fmt, ...);
-void log_warning(const char *const fmt, ...);
-void log_debug(const char *const fmt, ...);
-void log_info(const char *const fmt, ...);
-
-#endif /* LOG_H */
